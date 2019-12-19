3a3e854938471242254dcc10c3e0dc773101f17e
Botond Horvath <hello@boti7.com>
2019-12-19 19:07:37 +0100

    Add files generated by Spring Initializr

---
+++ src/main/java/com/ketfarkukutya/GoodDeedServer/GoodDeedServerApplication.java
@@ -0,0 +1,13 @@
+package com.ketfarkukutya.GoodDeedServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodDeedServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodDeedServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ketfarkukutya/GoodDeedServer/GoodDeedServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ketfarkukutya.GoodDeedServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodDeedServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
